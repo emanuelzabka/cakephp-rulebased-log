143dd257b7047fcf9f453d2da8fb2f92cff95e29
Gustavo Dutra <gustavotkg@gmail.com>
2011-04-28 17:21:28 -0300

    Compenent is now more generic and easier to use.

--- controllers/components/logger.php
+++ controllers/components/logger.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * $Id$
  * Component for automatic logging in database depending on controller statuses
- * Exemplo de configurações:
+ * Setting example:
  *   array(
  *	'states' => array(
  *		'controller' => 'name',
@@ -42,20 +41,44 @@
  *	)
  * 
  * @uses Object
- * @package Core
- * @subpackage Component
- * @version $Rev$
- * @modified $Date$
- * @modifiedby $Author$
  */
 class LoggerComponent extends Object {
 
 	public $controller = null;
 	public $settings = array();
+	public $builtinStates = Array(
+		'plugin' => 'plugin',
+		'controller' => 'name',
+		'action' => 'action',
+		'post' => array('isPost'),
+		'status' => 'actionStatus'
+	);
+	public $defaultLogAction = 'logAction';
+	public $builtinRules = Array(
+		'add' => array(
+			'state' => array('action' => 'add', 'post' => true, 'status' => 'success'),
+			'message' => 'added new :what'
+		),
+		'edit' => array(
+			'state' => array('action' => 'edit', 'post' => true, 'status' => 'success'),
+			'message' => ':what edited'
+		),
+		'delete' => array(
+			'state' => array('action' => 'delete', 'post' => false),
+			'message' => ':what removed'
+		)
+	);
+	public $defaultSessionLoginPath = 'User.login';
 
-	public function initialize($controller, $settings = array()) {
+	public function initialize($controller) {
 		$this->controller = $controller;
-		$this->settings = $settings;
+		$defaultSettings = Array(
+			'states' => $this->builtinStates,
+			'logAction' => $this->defaultLogAction,
+			'rules' => $this->builtinRules,
+			'login_path' => $this->defaultSessionLoginPath
+		);
+		$this->settings = array_merge($defaultSettings, (array)Configure::read('Log.settings'));
 		// Adds/Override aditional settings to the component
 		if (!empty($this->controller->logAdditionalSettings)) {
 			$this->settings = array_merge_recursive($this->settings, $this->controller->logAdditionalSettings);
@@ -76,30 +99,26 @@ class LoggerComponent extends Object {
 
 	/**
 	 * Verifies if the state matches with the value
-	 * There are 3 types of parameter:
+	 * There are 2 types of parameter:
 	 * - A string with the name of any attribute of the controller class
 	 * - An array with any method of the controller, so the returning value can be compared
-	 * - An array with any boolean method of the controller. This method must start with '?'
 	 * @param mixed $param Param
 	 * @param mixed $value Value being compared
 	 * @return boolean
 	 */
 	protected function matchStateParamValue($param, $value) {
-		$result = false;
 		if (is_array($param)) {
 			$method = array_shift($param);
-			if ($method[0] != '?') {
-				$result = call_user_func_array(array($this->controller, $method), $param) == $value;
+			if (method_exists($this->controller, $method)) {
+				$result = call_user_func_array(array($this->controller, $method), $param);
 			} else {
-				// Adiciona o valor para a callback
-				array_unshift($param, $value);
-				$result = call_user_func_array(array($this->controller, substr($method,1)), $param);
+				$result = call_user_func_array(array($this, $method), $param);
 			}
+			$result = ($result == $value);
 		} else {
-			$atr_val = null;
-			// Atributo no controller
+			// It's an attribute
 			$atrval = $this->controller->{$param};
-			$result = $atrval == $value;
+			$result = ($atrval == $value);
 		}
 		return $result;
 	}
@@ -124,15 +143,19 @@ class LoggerComponent extends Object {
 	 */
 	public function getMatchedRules() {
 		$result = array();
-		foreach ($this->settings['rules'] as $name => $rule) {
-			$rule['name'] = $rule;
-			if ($this->stateMatch($rule['state'])) {
-				// Se não for exclusiva a regra adiciona a lista de regras, senão deve apenas adicionar a regra atual
-				if (!isset($rule['exclusive']) || $rule['exclusive']) {
+		foreach ((array)@$this->settings['rules'] as $name => $rule) {
+			$rule['name'] = $name;
+			if (!array_key_exists('matched', $rule)) {
+				$this->settings['rules'][$name]['matched'] = false;
+				$rule['matched'] = false;
+			}
+			if (!$rule['matched'] && $this->stateMatch($rule['state'])) {
+				$this->settings['rules'][$name]['matched'] = true;
+				// If it is not exclusive, all the rules matching will be returned. Otherwise, only the first matching rule will be returned.
+				if (!isset($rule['exclusive']) || !$rule['exclusive']) {
 					$result[] = $rule;
 				} else {
 					$result = array($rule);
-					// É regra exclusiva então deve retornar apenas essa
 					break;
 				}
 			}
@@ -146,7 +169,10 @@ class LoggerComponent extends Object {
 	public function executeLog($rule) {
 		if (!empty($this->settings['logAction'])) {
 			$logAction = $this->settings['logAction'];
-			$this->controller->$logAction($rule);
+			if (method_exists($this->controller, $logAction))
+				$this->controller->$logAction($rule);
+			else
+				$this->$logAction($rule);
 		}
 	}
 
@@ -160,97 +186,37 @@ class LoggerComponent extends Object {
 		}
 	}
 
-	/**
-	 * Retorna os valores para a qual os wildcards das mensagens de log devem ser traduzidos 
-	 * Returns the values to which wildcards must be translated
-	 * Output example:
-	 * array(
-	 *   'name' => 'User.name'
-	 * )
-	 * @param array $datum Log line
-	 * @return array
-	 */
-	public function getWildcards($datum) {
-		/** FIXME: we should change module term for "plugin" **/
-		$module = ucfirst(low($datum['Log']['module']));
-		// Array contendo o nome descritivo dos controllers do módulo
-		$objects = Configure::read('Log.objects.'.$module);
-		$what = $datum['Log']['controller'];
-		if (!empty($objects[$datum['Log']['controller']])) {
-			$what = $objects[$datum['Log']['controller']];
-		}
-		$result = Array(
-			'Module' => $datum['Log']['module'],
-			'What' => $what
-		);
-		
-		// Looks for controller values
-		$module_wildcards = (array)Configure::read('Log.wildcards.'.$module);
-		$controller_wildcards = (array)@$module_wildcards[$datum['Log']['controller']];
-		foreach ($controller_wildcards as $wc => $wc_value) {
-			if (empty($result[$wc])) {
-				$result[$wc] = $wc_value;
-			}
-		}
-		$values = (array)@$datum['Log']['data']['data'];
-		// Looks for values for plugin
-		if (empty($result['name'])) {
-			$modelClass = $datum['Log']['model_class'];
-			foreach ((array)@$module_wildcards['All'] as $wc_value) {
-				if (Set::check($values, $modelClass.'.'.$wc_value)) {
-					$result['name'] = ':'.$modelClass.'.'.$wc_value;
-					break;
-				}
-			}
-		}
-		// Looks for values for project
-		$wildcards_all = (array)Configure::read('Log.wildcards.All');
-		if (empty($result['name'])) {
-			$modelClass = $datum['Log']['model_class'];
-			foreach ($wildcards_all as $wc_value) {
-				if (Set::check($values, $modelClass.'.'.$wc_value)) {
-					$result['name'] = ':'.$modelClass.'.'.$wc_value;	
-					break;
-				}
-			}
-		}
-		return $result;
+	public function formatMessage($msg) {
+		$objects = Configure::read('Log.objects.All');
+		if ($this->controller->plugin)
+			$objects = array_merge($objects, Configure::read('Log.objects.'.$this->controller->plugin));
+
+		$what = $this->controller->name;
+		if (!empty($objects[$what]))
+			$what = $objects[$what];
+
+		return String::insert($msg, array('what' => $what));
 	}
 
-	/**
-	 * Translates log messages and replaces metatags/wildcards with log definitions
-	 * Considers settings arrays for controller, plugin and project tags
-	 * Settings array usage example:
-	 * Configure::write('Log.wildcards.<Plugin name>', array(
-	 * 	// Controller specific wildcards
-	 * 	'User' => array(
-	 * 		'name' => ':User.name (:User.login)'
-	 * 	),
-	 *  // Values for :name tag in log messages, excep User in User controller which is overriden for the up example
-	 * 	'All' => array(
-	 * 		'name', 'description'
-	 * 	));
-	 * Tags for the whole project can be set in Log.wildcards.All param
-	 * Configure::write('Log.wildcards.All', array('name', 'description'));
-	 * @param mixed $data Log messages
-	 * @return array
-	 */
-	public function parseMessages($data) {
-		foreach ($data as &$datum) {
-			$log = $datum['Log']['message'];
-			$log_data = $datum['Log']['data'] = json_decode($datum['Log']['data'],true);
-			$wildcards = $this->getWildcards($datum);
-			$message = Configure::read('Log.messages.'.$log);
-			if (!empty($message)) {
-				$message = String::insert($message,$wildcards);
-			} else {
-				$message = $log;
-			}
-			$datum['Log']['message'] = String::insert($message, Set::flatten((array)@$log_data['data']));
-		}
-		return $data;
+	public function logAction($rule) {
+		$Log = ClassRegistry::init('Rblog.Log');
+		$Log->create();
+		$Log->save(Array(
+			'plugin' => (is_null($this->controller->plugin) ? 'App' : $this->controller->plugin),
+			'controller' => $this->controller->name,
+			'model_class' => $this->controller->modelClass,
+			'user_login' => $this->controller->Session->read($this->settings['login_path']),
+			'message' => $this->formatMessage($rule['message']),
+			'ip' => 'localhost'
+		));
+	}
+
+	public function isPost($param = null) {
+		return in_array($_SERVER['REQUEST_METHOD'], array('POST','PUT'));
+	}
+
+	public function __call($method, $args) {
+		return null;
 	}
 
 }
-
-?>
